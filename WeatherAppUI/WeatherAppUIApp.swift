acd78a46d452f650613cfa85f2f7cd3432a836c4
lukehkelly <lhkelly@unc.edu>
2025-09-30 15:20:55 -0400

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by Luke Kelly on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
